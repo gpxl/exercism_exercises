db163fb79a1a5cc99c6335f99c5209f376b3e5f9
Gerlando Piro <gerlando@gmail.com>
2015-10-27 12:22:38 -0700

    bit o cleanup

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,9 +1,9 @@
 class Grains
   def self.square(n)
-    2**(n-1)
+    2**(n - 1)
   end
 
   def self.total
-    (1..64).reduce(0){|sum, i| sum + square(i) }
+    (1..64).reduce(0) { |sum, i| sum + square(i) }
   end
 end
